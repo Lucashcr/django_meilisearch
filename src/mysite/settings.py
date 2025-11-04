3df4b8f9961599cf0043c814f7837dbecef58381
Lucas Rocha <lucash.rocha@hotmail.com>
2025-11-04 19:29:53 -0300

    Use python-dotenv and update python version constraints

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,9 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-SECRET_KEY = (
-    "django-insecure-hl)cj0%op2qjwr-7_935=jwfa%z!ptg8a4o0k^pcry$o-5&!g-"
-)
+SECRET_KEY = "django-insecure-hl)cj0%op2qjwr-7_935=jwfa%z!ptg8a4o0k^pcry$o-5&!g-"
 
 DEBUG = True
 
@@ -127,8 +131,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 DJANGO_MEILISEARCH = {
     "url": os.environ["MEILISEARCH_URL"],  # Your MeiliSearch host
-    "api_key": os.environ[
-        "MEILISEARCH_API_KEY"
-    ],  # Your MeiliSearch master key
+    "api_key": os.environ["MEILISEARCH_API_KEY"],  # Your MeiliSearch master key
     # "timeout": 1,  # Timeout for MeiliSearch requests (optional)
 }
