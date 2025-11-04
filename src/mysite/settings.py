f3308e8368a006de55dc115f821aff3246c37924
Lucas Rocha <lucash.rocha@hotmail.com>
2025-11-04 19:02:26 -0300

    Update ci

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -20,12 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = (
     "django-insecure-hl)cj0%op2qjwr-7_935=jwfa%z!ptg8a4o0k^pcry$o-5&!g-"
 )
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []  # type: list[str]
